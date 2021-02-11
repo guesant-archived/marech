4d51c40f117d76e1097aabad969581645d01661b
Gabriel Rodrigues <jipacoding@gmail.com>
2021-02-11 07:22:11 -0400

    feat(cli): makeConfig: generateRules: htmlImport

--- cli/src/makeConfig/generateRules.ts
+++ cli/src/makeConfig/generateRules.ts
@@ -1,8 +1,37 @@
+import { TransformerHTMLModule } from "@guesant/marech-transformer-html-modules";
+import { dirname } from "path";
 import { IConfigRule } from "../types/IConfigRule";
 import { IRulesPresetOptions } from "../types/IRulesPresetOptions";
+import { makeFileSystem } from "./makeFileSystem";
 
-export const generateRules = ({}: IRulesPresetOptions) => {
+export const generateRules = (
+  { mappedPaths = {}, htmlImport, htmlMinify, prettier }: IRulesPresetOptions,
+  defaultsEnabled = true,
+) => {
   const rules: IConfigRule[] = [];
+
+  if (htmlImport?.enabled ?? (true && defaultsEnabled)) {
+    const match = htmlImport?.match || "**/*.html";
+    rules.push({
+      match,
+      getTransformer: (
+        { fileContent, filePath, dependencyGraph },
+        configRules,
+      ) =>
+        new TransformerHTMLModule(
+          fileContent,
+          {
+            fs: makeFileSystem(
+              dirname(filePath),
+              mappedPaths,
+              configRules,
+              dependencyGraph,
+            ),
+          },
+          {},
+        ),
+    });
+  }
 
   return rules;
 };
--- cli/src/types/IRulesPresetOptions.ts
+++ cli/src/types/IRulesPresetOptions.ts
@@ -1,2 +1,10 @@
+import { IConfigRule } from "./IConfigRule";
+import { IMakeFileSystemMappedPaths } from "./IMakeFileSystemMappedPaths";
+
 export type IRulesPresetOptions = {
+  mappedPaths?: IMakeFileSystemMappedPaths;
+  htmlImport?: {
+    match?: IConfigRule["match"];
+    enabled?: boolean;
+  };
 };
